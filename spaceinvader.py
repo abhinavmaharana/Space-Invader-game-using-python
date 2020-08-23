b076bfcc326ea1bc1fce7adef47d7731fad266d9
abhinavmaharana <abhinavmaharana.004@gmail.com>
2020-08-23 14:46:06 +0530

    game layout complete

--- spaceinvader.py
+++ spaceinvader.py
@@ -144,3 +144,139 @@ def MYGAME():
         #-----------------------------------#
         #   game event handling ends here   #
         #-----------------------------------#
+
+        #----------------------------------------------------------------------------------------#
+        # Checking if enemy is hit by bullet if so replacing it from screen and increasing score #
+        #----------------------------------------------------------------------------------------#
+        if abs(int(bulletpos[0])-int(enemypos[0]))<=40 and abs(int(bulletpos[1])-int(enemypos[1]))<=75:
+            score+=1
+            tscore=str(score*10) 
+            SCREEN.blit(destroyimg,(bulletpos[0],bulletpos[1]-80))
+            pygame.display.update()
+            enemypos[0]=random.randint(100,SCREENWIDTH-100)
+            enemypos[1]=50
+            pygame.mixer.music.load("")
+            pygame.mixer.music.play()
+        
+        #---------------------------------------------#
+        # Giving a warning to player as enemy is near #
+        #---------------------------------------------#
+
+        if abs(int(playerpos[1])-int(enemypos[1]))==200:
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+
+        #---------------------------------------------#
+        #   Leveling up with changing enemy velocity  #
+        #---------------------------------------------#
+        if int(tscore)>=100 and temp==0:
+            temp=1
+            enemyvelocity=1.0
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+
+        elif int(tscore)>=200 and temp==1:      
+            temp=2
+            enemyvelocity=1.5
+            playervelocity=10
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+
+        elif int(tscore)>=300 and temp==2:
+            enemyvelocity=2.0
+            playervelocity=12
+            temp=3
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+            
+        elif int(tscore)>=400 and temp==3:
+            enemyvelocity=2.5
+            playervelocity=14
+            temp=4
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+            
+        elif int(tscore)>=500 and temp==4:
+            enemyvelocity=3.5
+            playervelocity=18
+            temp=5
+            pygame.mixer.music.load()
+            pygame.mixer.music.play()
+
+    #-------------------------------------------------------#
+    #        Changing bullet,player,enemy position          #
+    #-------------------------------------------------------#
+
+    bulletpos[1]+=bulletvelocity
+    playerpos[0]+=playervelocity
+    enemypos[1]+=enemyvelocity
+
+    #-----------------------------------------------------------------------------------------------------#
+    #   Checking if player is at extreme left and repositioning on screen if player going out of screen   #
+    #-----------------------------------------------------------------------------------------------------#
+
+    if playerpos[0]<=0:
+        playerpos[0]=50
+
+    #----------------------------------------------------------------------------#
+    # checking if player is at extreme right of screen and repositioning player  #
+    #----------------------------------------------------------------------------#
+
+    if playerpos[0]>=SCREENWIDTH:
+        playerpos[0]=-30
+
+    #----------------------------------------------------------------------------------#
+    # checking if enemy is about to approach the player if so  game over is to be done #
+    #----------------------------------------------------------------------------------#
+
+    if (enemypos[1]>=(SCREENHEIGHT-100)):
+        GAMEOVER()
+
+    #------------------------------------------------------------------------#
+    # blitting images of game background,player,bullet,enemy and setting FPS #
+    #------------------------------------------------------------------------#
+
+        SCREEN.blit(gamebackground,(0,0))
+        score_screen(f"SCORE:{tscore}",RED,450,30) 
+        SCREEN.blit(player,(playerpos[0],playerpos[1]))
+        SCREEN.blit(bullet,(bulletpos[0],bulletpos[1]))
+        SCREEN.blit(enemy,(int(enemypos[0]),int(enemypos[1])))
+        pygame.display.update()
+        FPSCLOCK.tick(FPS)
+
+def GAMEOVER():
+    pygame.mixer.music.load()
+    pygame.mixer.music.play()
+
+    playerpos=[500,530]
+    enemypos=[500,50]
+    bulletpos=[520,150]
+    
+    SCREEN.blit(gameoverbackground,(0,0))
+    SCREEN.blit(player,(playerpos[0],playerpos[1]))
+    SCREEN.blit(enemy,(enemypos[0],enemypos[1]))
+    SCREEN.blit(bullet,(bulletpos[0],bulletpos[1]))
+    pygame.display.update()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.mixer.music.load()
+                pygame.mixer.music.play()
+                time.sleep(2)
+                pygame.quit()
+                sys.exit()
+
+            if event.type==pygame.KEYDOWN and (event.key==pygame.K_UP or event.key==pygame.K_SPACE):
+                WELCOMESCREEN()
+                
+                
+    FPSCLOCK.tick(FPS)
+
+
+#----------------------------------------#
+#          Running the program           #
+#----------------------------------------#
+
+if __name__ == "__main__":
+    WELCOMESCREEN()
